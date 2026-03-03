16b0c178972ffecfa7d42360d24320b9b276de3b
Glauciene Luminato <seu-email@gmail.com>
2026-03-03 16:59:46 -0300

    atualizações no gerenciamento do estoque

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 class Produto(models.Model):
     nome = models.CharField(max_length=200)
@@ -15,12 +16,16 @@ class Venda(models.Model):
     total = models.DecimalField(max_digits=10, decimal_places=2, blank=True)
     data_venda = models.DateTimeField(auto_now_add=True)
 
-    def save(self,*args,**kwargs):
-       
-         self.total = self.produto.preco * self.quantidade
-         self.produto.estoque -= self.quantidade
-         self.produto.save()
-         super().save(*args,**kwargs)
+    
+    def clean(self):
+        if self.quantidade > self.produto.estoque:
+            raise ValidationError("Estoque insuficiente para realizar a venda.")
+    def save(self, *args,**kwargs):
+        self.full_clean()
+        self.total = self.produto.preco * self.quantidade
+        self.produto.estoque -= self.quantidade
+        self.produto.save()
+        super().save(*args,**kwargs)
 
     def __str__(self):
         return f"Vendas #{self.id}"
