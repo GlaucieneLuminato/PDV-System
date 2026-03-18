2924a5b8e1c405987c59f37545c645ad9958c3f6
Glauciene Luminato <seu-email@gmail.com>
2026-03-18 16:07:06 -0300

    nova integração ao backend

--- frontend/api.js
+++ frontend/api.js
@@ -1,45 +1,31 @@
-const API_URL = "http://127.0.0.1:8000/api/produtos/";
-
-let carrinho = [];
+const API_URL = "http://127.0.0.1:8000/api/produtos/"
 
 async function carregarProdutos(){
     try{
+        const container = document.querySelector("#gridView");
+
+        if(!container){
+            console.error("Elemento #gridView não encontrado!");
+            return;
+        }
         const resposta = await fetch(API_URL);
         const produtos = await resposta.json();
-        console.log(produtos);
-        console.log("API conectada BABY!!");
 
-        const tabela = document.querySelector("#tabela-produtos");
-        tabela.innerHTML = "";
+        container.innerHTML = "";
 
-        produtos.forEach(produto => {
-            const linha = `
-            <tr>
-                <td>${produto.nome}</td>
-                <td>${produto.id}</td>
-                <td>-</td>
-                <td>${produto.estoque}</td>
-                <td>-</td>
-                <td>R$ ${produto.preco}</td>
-                <td>ativo</td>
-                <td>
-                <button onClick="adicionarCarrinho(${produto.id})" style="border:none;background:none">
-                <i class="bi bi-cart-plus"></i>
-                </button>
-                </td>
-            </tr>
+        produtos.forEach(produtos => {
+            const card = `
+            <div class="product-card">
+            <h5>${produtos.nome}</h5>
+            <p>Preço: £ ${produtos.preco}</p>
+            <p>Estoque: ${produtos.estoque}</p>
+            </div>
             `;
-            tabela.innerHTML  += linha
+            container.innerHTML += card;
         });
-    } catch(erro){
-        console.error("Erro ao carregar produto:", erro);
+    } catch(error){
+        console.error("Erro ao carregar produto:", error);
     }
 }
 
-function adicionarCarrinho(id){
-    carrinho.push(id);
-    console.log("Carrinho:",carrinho);
-}
-
-carregarProdutos();
-
+window.onload = carregarProdutos;
