f3ef9b16eda7fbe380c74d107eaeb25e58a29357
Glauciene Luminato <seu-email@gmail.com>
2026-03-20 19:56:54 -0300

    "feat: implementação completa do CRUD de produtos com integração ao Django API + autenticação JWT
    - CRUD completo (create, read, update, delete)
    - Integração com API Django REST
    - Correção de erros 400 (preco obrigatório)
    - Implementação de autenticação JWT (login com token)
    - Adição de Authorization Bearer nas requisições
    - Sistema de busca de produtos (nome, categoria, SKU)
    - Feedback visual com Toastify (create/update/delete)
    - Correção de renderização e variáveis (listaProdutos)
    - Limpeza e organização do código frontend
    
    Sistema pronto para próxima etapa: proteção de rotas e módulo de vendas (PDV)"

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -3,6 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views_api import ProdutoViewSet, VendaViewSet, ItemVendaViewSet
 from django.urls import path
 from .views_api import dashboard
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 router = DefaultRouter()
 router.register(r'produtos',ProdutoViewSet)
@@ -12,4 +13,6 @@ router.register(r'itens',ItemVendaViewSet)
 urlpatterns = [
     path('',include(router.urls)),
     path("dashboard/", dashboard, name="dashboard"), 
+    path('login/',TokenObtainPairView.as_view(), name='login'),
 ]
+
