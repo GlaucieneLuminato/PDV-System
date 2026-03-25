d42e6530bf964b4b1f29421df19772d7f51164d4
𝐺𝑙𝑎𝑢𝑐𝑖𝑒𝑛𝑒 𝐿𝑢𝑚𝑖𝑛𝑎𝑡𝑜 👩‍💻 <162189425+GlaucieneLuminato@users.noreply.github.com>
2026-03-25 17:25:11 -0300

    Update settings.py

--- backend/pdv/settings.py
+++ backend/pdv/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3&m4vmw5fk))ip9!t@n-bf(faak0n!-fk+y2wvc(vkotx0+v(o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -51,10 +52,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    
 ]
 
-ROOT_URLCONF = 'pdv.urls'
+ROOT_URLCONF = 'backend.pdv.urls'
 
 TEMPLATES = [
     {
@@ -71,7 +72,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'pdv.wsgi.application'
+WSGI_APPLICATION = 'backend.pdv.wsgi.application'
 
 
 # Database
@@ -124,8 +125,8 @@ STATIC_URL = 'static/'
 CORS_ALLOW_ALL_ORIGINS = True
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHETICATION_CLASSES': (
-        'rest_framework_simplejwt.authetication.JWTAuthentication',
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
 }
 
