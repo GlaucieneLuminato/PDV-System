e28f89eaec7c9baf8bc0c0a34e224912c4f2cb91
Glauciene Luminato <seu-email@gmail.com>
2026-03-27 18:55:30 -0300

    "fix: configurar arquivos estáticos"

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.core.exceptions import ValidationError
 from decimal import Decimal
+from django.contrib.auth.models import User
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
 class Produto(models.Model):
     nome = models.CharField(max_length=200)
@@ -61,7 +64,23 @@ class ItemVenda(models.Model):
                     
     def __str__(self):
                     return f"{self.produto.nome} - {self.quantidade}"
+                
+
         
+class Funcionario(models.Model):
+    
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    telefone = models.CharField(max_length=15)
+    data_nascimento = models.DateField()
+    cpf = models.CharField(max_length=14)
+    
+    is_admin = models.BooleanField(default=False)
+    def __str__(self):return self.user.username
 
-
+class Usuario(AbstractBaseUser):
+    TIPO_USUARIO = (
+        ('admin', 'Administrador'),
+        ('funcionario','Funcionario'),
+    )
+    tipo = models.CharField(max_length=20, choices=TIPO_USUARIO)
 # Create your models here.
