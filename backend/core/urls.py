20ccea280c0b461f0a20a1b8cd45cba87ca49109
Glauciene Luminato <seu-email@gmail.com>
2026-03-31 13:46:57 -0300

    "fix: algumas correções no urls e views"

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,26 +1,19 @@
-from django .urls import path, include
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views_api import ProdutoViewSet, VendaViewSet, ItemVendaViewSet
-from django.urls import path
-from .views_api import dashboard
+from .views_api import ProdutoViewSet, VendaViewSet, ItemVendaViewSet, dashboard, UserViewSet
 from rest_framework_simplejwt.views import TokenObtainPairView
-from .views_api import UserViewSet
 from .views import me
 
 router = DefaultRouter()
-router.register(r'produtos',ProdutoViewSet)
-router.register(r'vendas',VendaViewSet)
-router.register(r'itens',ItemVendaViewSet)
-router.register(r'users',UserViewSet)
+router.register(r'produtos', ProdutoViewSet)
+router.register(r'vendas', VendaViewSet)
+router.register(r'itens', ItemVendaViewSet)
+router.register(r'users', UserViewSet)
 
-
-    
 urlpatterns = [
-    
     path("dashboard/", dashboard, name="dashboard"), 
-    path('login/',TokenObtainPairView.as_view(),name='login'),
-    path('me/',me),
-   
+    path("login/", TokenObtainPairView.as_view(), name="login"),
+    path("me/", me),
+
     path("", include(router.urls)),
 ]
-
