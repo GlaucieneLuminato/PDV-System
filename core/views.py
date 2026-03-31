97510f8636cee1c93cb629b5c8434938dde52296
Glauciene Luminato <seu-email@gmail.com>
2026-03-31 13:45:24 -0300

    fix: separação correta de urls e views + correção rota /me

--- core/views.py
+++ core/views.py
@@ -1,31 +1,20 @@
-from django.shortcuts import render
-from django.contrib.auth import authenticate
 from rest_framework.response import Response 
 from rest_framework.decorators import api_view, permission_classes
-from django.contrib.auth.models import User
 from rest_framework.permissions import IsAuthenticated
 
-
-
-@api_view(['POST'])
-def criar_funcionario(request):
-    username = request.data.get('username')
-    password = request.data.get('password')
-    
-    user = User.objects.create_user(
-        username = username,
-        password = password,
-       
-    )
-    
-    user.is_staff = True
-    user.save()
-    
-    return Response({'msg':'Funcionário criado'})
-
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def me(request):
-    return Response({"msg":"ok"})
-         
-        
-# Create your views here.
+    user = request.user
+
+    if user.is_superuser:
+        tipo = "admin"
+    elif user.is_staff:
+        tipo = "funcionario"
+    else:
+        tipo = "usuario"
+
+    return Response({
+        "username": user.username,
+        "tipo": tipo
+    })
