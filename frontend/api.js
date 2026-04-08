16ef470885e05d2b96ab81d37e8fe62bfd355f3a
Glauciene Luminato <seu-email@gmail.com>
2026-04-08 12:23:17 -0300

    fix: correções necessárias

--- frontend/api.js
+++ frontend/api.js
@@ -1,9 +1,9 @@
 const API_URL = "https://pdv-system-c359.onrender.com/api/produtos/";
 const API_BASE = "https://pdv-system-c359.onrender.com";
 const API_PRODUTOS = API_BASE + "/api/produtos/";
-const API_VENDAS = API_BASE + "vendas/";
-const API_USERS = API_BASE + "users/";
-const API_LOGIN = API_BASE + "login/";
+const API_VENDAS = API_BASE + "/api/vendas/";
+const API_USERS = API_BASE + "/api/users/";
+const API_LOGIN = API_BASE + "/api/login/";
 
 
 
