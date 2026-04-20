baf28f23aa93958f2cfb9580d11b0fc3c731ec27
Glauciene Luminato <seu-email@gmail.com>
2026-04-20 12:39:44 -0300

    adicionando autenticação JWT

--- pdv/urls.py
+++ pdv/urls.py
@@ -15,12 +15,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 
+# 🔐 IMPORTANTE (adiciona isso)
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # SUAS ROTAS
     path("api/", include('core.urls')),
     path("", include('core.urls')),
-    
+
+    # 🔐 ROTAS DE AUTENTICAÇÃO (ESSENCIAL)
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
